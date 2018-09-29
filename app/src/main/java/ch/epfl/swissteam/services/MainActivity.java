e97c28cb62038ad5f7348ccbce2bffd2a30b4dc8
abaudat <baudatadrian@gmail.com>
2018-09-29 14:43:58 +0200

    Add design for NewProfileDetails actvity

--- app/src/main/java/ch/epfl/swissteam/services/MainActivity.java
+++ app/src/main/java/ch/epfl/swissteam/services/MainActivity.java
@@ -1,5 +1,6 @@
 package ch.epfl.swissteam.services;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: remove before pushing
+        Intent i = new Intent(this, NewProfileDetails.class);
+        startActivity(i);
     }
 }
