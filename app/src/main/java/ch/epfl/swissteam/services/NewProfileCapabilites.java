60af1a65469fa0024a8400b867fd845da8648aa3
sgachoud <sebastien.gachoud@epfl.ch>
2018-09-29 11:05:15 +0200

    added a button to add service, and a test to try to add a button dynamically

--- app/src/main/java/ch/epfl/swissteam/services/NewProfileCapabilites.java
+++ app/src/main/java/ch/epfl/swissteam/services/NewProfileCapabilites.java
@@ -2,6 +2,10 @@ package ch.epfl.swissteam.services;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class NewProfileCapabilites extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class NewProfileCapabilites extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_profile_capabilites);
+    }
+
+    public void addNewActivity(View view) {
+        Button testButton = new Button(this);
+        testButton.setText("test");
+        LinearLayout ll = findViewById(R.id.mainScrollViewLayout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                                                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                                                ViewGroup.LayoutParams.WRAP_CONTENT
+                                                                );
+        ll.addView(testButton, lp);
     }
 }
