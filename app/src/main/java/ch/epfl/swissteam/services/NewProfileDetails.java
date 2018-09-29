310df85b598f3c1e485f617d6756539aaad78a64
abaudat <baudatadrian@gmail.com>
2018-09-29 18:42:37 +0200

    Complete functionality of NewProfileDetails activity
    User can pick a new picture from their device. Add ImageUtil class containing
    useful static methods for manipulating images and bitmaps.
    
    See #21

--- app/src/main/java/ch/epfl/swissteam/services/NewProfileDetails.java
+++ app/src/main/java/ch/epfl/swissteam/services/NewProfileDetails.java
@@ -1,12 +1,22 @@
 package ch.epfl.swissteam.services;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * This activity is used to set up the account details of a new user.
@@ -19,6 +29,7 @@ public class NewProfileDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_profile_details);
+        setTitle(getResources().getString(R.string.newprofile_title));
     }
 
     /**
@@ -31,17 +42,57 @@ public class NewProfileDetails extends AppCompatActivity {
     }
 
     /**
-     * Sets the picture in the picture <code>ImageView</code>
+     * Sets the picture in the <code>ImageView</code>
      *
-     * @param img new image
+     * @param img new image bitmap
      */
-    private void setPicture(Drawable img) {
-        ((ImageView)findViewById(R.id.imageview_newprofiledetails_picture)).setImageDrawable(img);
+    private void setPicture(Bitmap img) {
+        ((ImageView)findViewById(R.id.imageview_newprofiledetails_picture)).setImageBitmap(img);
     }
 
+    /**
+     * Goes to the capabilities activity.
+     *
+     * @param view view
+     */
     public void nextScreen(View view) {
         //TODO: Save changes made by the user here.
         Intent intent = new Intent(this, NewProfileCapabilities.class);
         startActivity(intent);
     }
+
+    /**
+     * Changes the picture by allowing the user to fetch a picture from his device.
+     *
+     * @param view view
+     */
+    public void changePicture(View view) {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        galleryIntent.putExtra("return-data", true);
+        startActivityForResult(galleryIntent, 1);
+    }
+
+    /**
+     * Sets the picture bitmap to a bitmap obtained by letting the user choose an image on his device.
+     * This will rotate the bitmap so that it is upright.
+     *
+     * @param requestCode request code
+     * @param resultCode result code
+     * @param data data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                bitmap = ImageUtils.modifyOrientation(bitmap, getContentResolver().openInputStream(data.getData()));
+                setPicture(bitmap);
+            } catch (IOException e) {
+                Log.w("BITMAP_CREATING", "Problem creating bitmap from activity result.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
