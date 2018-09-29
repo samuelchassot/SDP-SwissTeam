edcc7440f4e89d0b4aeef301f3c5e74eccb4cf95
sgachoud <sebastien.gachoud@epfl.ch>
2018-09-29 14:45:28 +0200

    added a new CapabilitySelection class to implement the visual selection of capabilities

--- app/src/main/java/ch/epfl/swissteam/services/NewProfileCapabilities.java
+++ app/src/main/java/ch/epfl/swissteam/services/NewProfileCapabilities.java
@@ -4,9 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class NewProfileCapabilities extends AppCompatActivity {
 
@@ -16,19 +21,71 @@ public class NewProfileCapabilities extends AppCompatActivity {
         setContentView(R.layout.activity_new_profile_capabilites);
     }
 
-    public void addNewActivity(View view) {
-        Spinner newSpinner = new Spinner(this);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+    public void addNewCapability(View view) {
+        //retrieving the LinearLayout of the mainScroll
+        LinearLayout ll = findViewById(R.id.mainScrollViewLayout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        ll.addView(new CapabilitySelection(this), lp);
+    }
+
+}
+
+class CapabilitySelection extends TableLayout
+                            implements AdapterView.OnItemSelectedListener {
+
+    public CapabilitySelection(NewProfileCapabilities context){
+        super(context);
+        NPC_ = context;
+        generateCapability();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        generateSubcat();
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
+
+    private void generateCapability(){
+        Spinner newSpinner = new Spinner(NPC_);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(NPC_,
                 R.array.domains_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         newSpinner.setAdapter(adapter);
+        newSpinner.setOnItemSelectedListener(this);
+        addView(newSpinner);
 
-        LinearLayout ll = findViewById(R.id.mainScrollViewLayout);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                                                                ViewGroup.LayoutParams.MATCH_PARENT,
-                                                                ViewGroup.LayoutParams.WRAP_CONTENT
-                                                                );
+        //creating keywords field
+        keyWordField_ = new EditText(NPC_);
+        subcatSpinner_ = new Spinner(NPC_);
 
-        ll.addView(newSpinner, lp);
+
+        TableRow newTableRow = new TableRow(NPC_);
+        newTableRow.addView(subcatSpinner_);
+        newTableRow.addView(keyWordField_);
+        addView(newTableRow);
     }
+
+    public void generateSubcat(){
+        //creating the spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(NPC_,
+                R.array.subcat_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        subcatSpinner_.setAdapter(adapter);
+        keyWordField_.setHint(R.string.enter_key_word);
+
+    }
+
+    private NewProfileCapabilities NPC_;
+    private Spinner subcatSpinner_;
+    private EditText keyWordField_;
 }
