d3531180d953c9ef59d0adefcf83b25aeee8ee3a
Ghali C <ghalicb@gmail.com>
2018-10-03 07:03:55 +0200

    => Creation of MainActivityTest

--- app/src/androidTest/java/ch/epfl/swissteam/services/SignInActivityTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/SignInActivityTest.java
@@ -15,7 +15,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 public class SignInActivityTest {
     @Rule
-    public final ActivityTestRule<SignInActivity> mActivityRule =
+    public final ActivityTestRule<SignInActivity> signInActivityRule_ =
             new ActivityTestRule<>(SignInActivity.class);
 
     @Test
