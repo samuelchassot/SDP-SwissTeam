6d61928a23dd2f05d2b9b096b18915afa5907def
Ghali C <ghalicb@gmail.com>
2018-10-03 05:16:04 +0200

    => Add uncomplete signOut before merge
    Add the principal element for the logout, to be completed with what was done in the login branch.

--- app/src/main/java/ch/epfl/swissteam/services/MainActivity.java
+++ app/src/main/java/ch/epfl/swissteam/services/MainActivity.java
@@ -91,27 +91,19 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        /*
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-        */
-        if(id == R.id.button_maindrawer_logout){
-            //here handle the logout click and call the logout method
+        switch (id) {
+            case (R.id.button_maindrawer_logout) :
+                signedOut();
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut();
     }
 }
