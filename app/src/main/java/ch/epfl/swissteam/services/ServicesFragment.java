0acb96eec819164de2eca6fc9935e7e08db4f8f1
Unknown <simon.wicky@epfl.ch>
2018-10-07 12:37:04 +0200

    Started recyclerview

--- app/src/main/java/ch/epfl/swissteam/services/ServicesFragment.java
+++ app/src/main/java/ch/epfl/swissteam/services/ServicesFragment.java
@@ -4,12 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 public class ServicesFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public ServicesFragment() {
         // Required empty public constructor
@@ -30,6 +36,12 @@ public class ServicesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_services, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstance){
+        mRecyclerView = getView().findViewById(R.id.services_recycler);
+        mLayoutManager = new LinearLayoutManager(getActivity());
     }
 
 }
