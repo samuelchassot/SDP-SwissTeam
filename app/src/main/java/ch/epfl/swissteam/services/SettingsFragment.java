8c7055e90f7b39d21fe67c6af1ccf0e7981fca99
Ghali C <ghalicb@gmail.com>
2018-10-10 15:01:48 +0200

    => Put a natural translation on the app (French and English) Remove the choice of language in the settings fragment, but let the two strings.xml files to let the application be in French or English according to the device settings of the user.

--- app/src/main/java/ch/epfl/swissteam/services/SettingsFragment.java
+++ app/src/main/java/ch/epfl/swissteam/services/SettingsFragment.java
@@ -1,8 +1,11 @@
 package ch.epfl.swissteam.services;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,9 +17,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class SettingsFragment extends Fragment {
-
-    private Spinner language_;
-    private ArrayAdapter<String> adapter_;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -32,45 +32,10 @@ public class SettingsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    //@Override
-    //protected void attachBaseContext(Context newBase) {
-      //  super.attachBaseContext(LanguageHelper.onAttach(newBase));
-    //}
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-        language_ = (Spinner) view.findViewById(R.id.language_spinner);
-        adapter_ = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.language_arrays));
-        language_.setAdapter(adapter_);
-
-        if (LanguageHelper.getLanguage(getActivity()).equalsIgnoreCase("en")) {
-            language_.setSelection(adapter_.getPosition("English"));
-        } else if (LanguageHelper.getLanguage(getActivity()).equalsIgnoreCase("fr")) {
-            language_.setSelection(adapter_.getPosition("French"));
-        }
-
-        language_.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                switch (i) {
-                    case 0:
-                        LanguageHelper.setLocale(getActivity(), "en");
-                        break;
-                    case 1:
-                        LanguageHelper.setLocale(getActivity(), "fr");
-                        Log.e("LANGUAGE", "French selected");
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
         return view;
     }
 }
