fc93d6acc1d286d92b8302a4acf70636ac42f6bd
abaudat <baudatadrian@gmail.com>
2018-10-11 17:52:41 +0200

    Save capabilities to the DB on profile creation

--- app/src/main/java/ch/epfl/swissteam/services/NewProfileCapabilities.java
+++ app/src/main/java/ch/epfl/swissteam/services/NewProfileCapabilities.java
@@ -1,85 +1,61 @@
 package ch.epfl.swissteam.services;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This activity is meant to fill the user profile with service capabilities at the first connection
  *
  * @author Sébastien Gachoud
+ * @author Adrian Baudat
  */
 
 public class NewProfileCapabilities extends AppCompatActivity {
+
+    List<Categories> capabilitiesList_ = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_profile_capabilites);
+
+        RecyclerView recycler = findViewById(R.id.recyclerview_newprofilecapabilities_list);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(new CategoriesAdapter(Categories.values()));
     }
 
-    public  void nextPage(View view) {
+    /**
+     * Save the categories the user checked to the DB and go to the next screen
+     *
+     * @param view view
+     */
+    public void nextPage(View view) {
+        for(Categories capability : capabilitiesList_) {
+            DBUtility.get().getDb_().child(DBUtility.CATEGORIES).child(capability.toString()).child(GoogleSignInSingleton.get().getClientUniqueID()).setValue("true");
+        }
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void addNewCapability(View view) {
-        //retrieving the LinearLayout of the mainScroll
-        LinearLayout ll = findViewById(R.id.main_scroll_view_layout);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        );
-
-        ll.addView(new CapabilitySelection(this), lp);
+    /**
+     * Add a capability to the user.
+     *
+     * @param capability capability to add.
+     */
+    public void addCapability(Categories capability) {
+        capabilitiesList_.add(capability);
     }
-
-
-    private Toolbar toolbar_;
-
-}
-
-@SuppressLint("ViewConstructor")
-class CapabilitySelection extends TableLayout {
-
-    public CapabilitySelection(NewProfileCapabilities context){
-        super(context);
-        NPC_ = context;
-        generateCapability();
-    }
-
-    private void generateCapability(){
-
-        //main spinner
-        Spinner newSpinner = new Spinner(NPC_);
-        ArrayAdapter<Categories> adapter = new ArrayAdapter<Categories>(getContext(), android.R.layout.simple_spinner_item, Categories.values());
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        newSpinner.setAdapter(adapter);
-        addView(newSpinner);
-
-
-        //creating keywords field
-        keyWordField_ = new EditText(NPC_);
-        keyWordField_.setHorizontallyScrolling(true);
-
-        TableRow newTableRow = new TableRow(NPC_);
-        newTableRow.addView(keyWordField_);
-
-        addView(newTableRow);
-    }
-
-    private final NewProfileCapabilities NPC_;
-    private EditText keyWordField_;
 }
