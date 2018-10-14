89716c5252a3f90a91c711cb8cfaedc25d9014b9
abaudat <baudatadrian@gmail.com>
2018-10-14 13:09:52 +0200

    Change NewProfileCapabilities and associated
    Rewrite NewProfileCapabilities, do saving of user in DB only there and change
    layout to a RecyclerView.

--- app/src/androidTest/java/ch/epfl/swissteam/services/NewProfileCapabilitiesTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/NewProfileCapabilitiesTest.java
@@ -17,13 +17,11 @@ public class NewProfileCapabilitiesTest {
     @Rule
     public final IntentsTestRule<NewProfileCapabilities> mActivityRule =
             new IntentsTestRule<>(NewProfileCapabilities.class);
-
-    /*
+    
     @Test
     public void canCheckCapability() {
         onView(withId(R.id.recyclerview_newprofilecapabilities_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, clickChildViewWithId(R.id.checkbox_capabilitylayout_check)));
     }
-    */
 
     public static ViewAction clickChildViewWithId(final int id) {
         return new ViewAction() {
