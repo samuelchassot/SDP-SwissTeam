617061186b8475a4d20bd001b200d08b3210a7bb
Unknown <samuel.chassot@epfl.ch>
2018-10-14 17:58:18 +0200

    Change text in toolbar when opening profile fragment

--- app/src/main/java/ch/epfl/swissteam/services/ProfileDisplayFragment.java
+++ app/src/main/java/ch/epfl/swissteam/services/ProfileDisplayFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,6 +43,9 @@ public class ProfileDisplayFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.toolbar_profile);
+
     }
 
     @Override
