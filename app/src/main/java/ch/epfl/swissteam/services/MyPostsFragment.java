ec987b5fbde17d023dcdbb54200a7a190d5172d3
Julie Giunta <julie.giunta@epfl.ch>
2018-10-15 22:00:09 +0200

    Change request in DBUtility getUsersPosts

--- app/src/main/java/ch/epfl/swissteam/services/MyPostsFragment.java
+++ app/src/main/java/ch/epfl/swissteam/services/MyPostsFragment.java
@@ -1,18 +1,14 @@
 package ch.epfl.swissteam.services;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
