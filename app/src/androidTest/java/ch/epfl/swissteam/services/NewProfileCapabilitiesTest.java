29041e2a55443118eb5091adef2325d9e88a8190
abaudat <baudatadrian@gmail.com>
2018-10-18 16:22:32 +0200

    Add test for ending profile creation

--- app/src/androidTest/java/ch/epfl/swissteam/services/NewProfileCapabilitiesTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/NewProfileCapabilitiesTest.java
@@ -7,17 +7,33 @@ import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.view.View;
 
 import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class NewProfileCapabilitiesTest {
     @Rule
     public final IntentsTestRule<NewProfileCapabilities> mActivityRule =
             new IntentsTestRule<>(NewProfileCapabilities.class);
-    
+
+    @Before
+    public void setDbOffline() {
+        TestUtils.setMock();
+    }
+
+    @Test
+    public void canGoToNext() {
+        onView(withId(R.id.button_newprofilecapabilites_done)).perform(click());
+        intended(hasComponent(MainActivity.class.getName()));
+    }
+
     @Test
     public void canCheckCapability() {
         onView(withId(R.id.recyclerview_newprofilecapabilities_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, clickChildViewWithId(R.id.checkbox_capabilitylayout_check)));
