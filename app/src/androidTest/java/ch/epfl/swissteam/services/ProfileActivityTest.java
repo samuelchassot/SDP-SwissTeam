958ee7646a06223b475134bfdde0cb644df1aa50
Ghali C <ghalicb@gmail.com>
2018-10-23 21:15:08 +0200

    => ActivityProfileTest working
    Need potentially to add the test for the imageURL

--- app/src/androidTest/java/ch/epfl/swissteam/services/ProfileActivityTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/ProfileActivityTest.java
@@ -2,6 +2,7 @@ package ch.epfl.swissteam.services;
 
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -16,21 +17,20 @@ import java.util.Arrays;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class ProfileActivityTest extends FirebaseTest {
 
-    private static final String mGoogleId_ = "1234";
-    private static final String oGoogleId_ = "456";
-    private static final String url_ = TestUtils.getATestUser().getImageUrl_();
-    private static final String name_ = "Mark", email_ = "mtSucre@fb.com", description_ = "Hi there, I'm using Socialize!";
+    private static final String M_GOOGLE_ID_ = "1234";
+    private static final String O_GOOGLE_ID_ = "456";
+    private static final String URL_ = TestUtils.getATestUser().getImageUrl_();
     private static final ArrayList<Categories> cats = new ArrayList<>(Arrays.asList(Categories.COOKING));
-    private static final User mUser_ = new User(mGoogleId_,"Bear", "polar@north.nth","",null, url_);
-    private static final User oUser_ = new User(oGoogleId_, "Raeb", "hairy@north.nth", "", cats, url_);
+    private static final User mUser_ = new User(M_GOOGLE_ID_,"Bear", "polar@north.nth","",null, URL_);
+    private static final User oUser_ = new User(O_GOOGLE_ID_, "Raeb", "hairy@north.nth", "", cats, URL_);
 
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
@@ -38,23 +38,23 @@ public class ProfileActivityTest extends FirebaseTest {
 
     @Override
     public void initialize() {
-        GoogleSignInSingleton.putUniqueID(mGoogleId_);
+        GoogleSignInSingleton.putUniqueID(M_GOOGLE_ID_);
         oUser_.addToDB(FirebaseDatabase.getInstance().getReference());
         mUser_.addToDB(FirebaseDatabase.getInstance().getReference());
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_services));
         try {
-            Thread.sleep(300);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_services));
     }
 
     @Test
     public void isTheNameInTheAssociatedTextView() {
         onView(withText(oUser_.getName_())).perform(click());
         try {
-            Thread.sleep(300);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -65,7 +65,7 @@ public class ProfileActivityTest extends FirebaseTest {
     public void isTheEmailInTheAssociatedTextView() {
         onView(withText(oUser_.getName_())).perform(click());
         try {
-            Thread.sleep(300);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -74,45 +74,46 @@ public class ProfileActivityTest extends FirebaseTest {
 
     @Test
     public void isTheDescriptionInTheAssociatedTextView() {
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         onView(withText(oUser_.getName_())).perform(click());
         try {
-            Thread.sleep(300);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         onView(withId(R.id.textView_profile_description)).check(matches(withText(oUser_.getDescription_())));
     }
-
+/*
     @Test
     public void isTheImageInTheAssociatedTextView() {
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         onView(withText(oUser_.getName_())).perform(click());
         try {
-            Thread.sleep(300);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         onView(withId(R.id.imageview_profile_picture)).check(matches(withText(oUser_.getImageUrl_())));
     }
-/*
+*/
     @Test
     public void canAccessToChatButtonIfOtherProfile() {
         onView(withText(oUser_.getName_())).perform(click());
-
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.button_profile_toChat)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
     @Test
     public void cantAccessToChatButtonIfMyProfile() {
         onView(withText(mUser_.getName_())).perform(click());
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.button_profile_toChat)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
 
-    }*/
+    }
 }
