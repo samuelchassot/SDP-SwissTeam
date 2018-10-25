f14207ea1eb1db72aee87f3e489773f3901c1897
Simon Wicky <simon.wicky@epfl.ch>
2018-10-25 19:48:03 +0200

    more tests

--- app/src/androidTest/java/ch/epfl/swissteam/services/ServicesFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/ServicesFragmentTest.java
@@ -9,10 +9,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+
 
 @RunWith(AndroidJUnit4.class)
 public class ServicesFragmentTest {
@@ -25,7 +32,7 @@ public class ServicesFragmentTest {
     public void openFragment() {
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_services));
-        onView(withId(R.id.services_spinner)).perform(click());
-
+        onView(withId(R.id.services_spinner)).perform(scrollTo()).perform(click());
+        onData(allOf(is(instanceOf(Categories.class)), is(Categories.HOUSE))).perform(click());
     }
 }
