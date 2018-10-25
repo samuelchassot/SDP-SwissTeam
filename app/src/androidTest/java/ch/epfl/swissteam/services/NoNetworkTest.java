ecde09a849b53f45467e19d1f8f5546d3db8b7c5
Simon Wicky <simon.wicky@epfl.ch>
2018-10-25 19:00:14 +0200

    airplane mode test, but can't get permission. Stand by

---
+++ app/src/androidTest/java/ch/epfl/swissteam/services/NoNetworkTest.java
@@ -0,0 +1,38 @@
+package ch.epfl.swissteam.services;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.os.SystemClock;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.telephony.TelephonyManager;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(AndroidJUnit4.class)
+public class NoNetworkTest {
+
+    @Rule
+    public final ActivityTestRule<MainActivity> mainActivityRule_ =
+            new ActivityTestRule<>(MainActivity.class);
+
+    //@Rule
+    //public GrantPermissionRule gpr = GrantPermissionRule.grant("android.permission.MODIFY_PHONE_STATE");
+
+    //@Rule
+    //public GrantPermissionRule gpr2 = GrantPermissionRule.grant("android.permission.CHANGE_WIFI_STATE");
+
+
+    @Test
+    public void cutNetwork() {
+        WifiManager wifi = (WifiManager) mainActivityRule_.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        //wifi.setWifiEnabled(false);
+        TelephonyManager tm = (TelephonyManager) mainActivityRule_.getActivity().getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        //tm.setDataEnabled(false);
+    }
+}
