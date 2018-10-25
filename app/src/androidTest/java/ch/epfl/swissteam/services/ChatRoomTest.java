29d0921f5752d74517f0a683a7d261807b862d72
abaudat <baudatadrian@gmail.com>
2018-10-25 19:14:09 +0200

    Move all unit tests to androidTest

--- app/src/androidTest/java/ch/epfl/swissteam/services/ChatRoomTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/ChatRoomTest.java
@@ -2,10 +2,12 @@ package ch.epfl.swissteam.services;
 
 import android.content.Intent;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -13,6 +15,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+@RunWith(AndroidJUnit4.class)
 public class ChatRoomTest {
     @Rule
     public final IntentsTestRule<ChatRoom> mActivityRule =
