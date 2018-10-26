8f4de48af1a2593e1c2963b6355a057bc3567823
sgachoud <sebastien.gachoud@epfl.ch>
2018-10-26 13:44:36 +0200

    added more sleep to ChatRoomTest

--- app/src/androidTest/java/ch/epfl/swissteam/services/ChatRoomTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/ChatRoomTest.java
@@ -46,7 +46,7 @@ public class ChatRoomTest extends FirebaseTest{
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_services));
         sleep(2000);
         recyclerScrollToItemWithTextAndPerformClickItem(R.id.services_recycler, TestUtils.O_USER.getName_());
-        sleep(2000);
+        sleep(4000);
         onView(withId(R.id.button_profile_toChat)).perform(scrollTo()).perform(click());
     }
 
