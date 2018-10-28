c62e1d789931aab49309620d601251fffe37b53a
Ghali C <ghalicb@gmail.com>
2018-10-28 15:43:29 +0100

    => Fix tests in CreatePostFragment

--- app/src/androidTest/java/ch/epfl/swissteam/services/CreatePostFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/CreatePostFragmentTest.java
@@ -22,6 +22,7 @@ import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static ch.epfl.swissteam.services.TestUtils.sleep;
 import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
@@ -44,7 +45,8 @@ public class CreatePostFragmentTest extends FirebaseTest{
     public void canOpenCreatePostFragment() {
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_myposts));
-        onView(withId(R.id.fragment_my_posts)).perform(click());
+        sleep(500);
+        onView(withId(R.id.floatingbutton_addpost)).perform(click());
         onView(withId(R.id.plaintext_createpostfragment_title)).perform(typeText(title)).check(matches(withText(title)));
         onView(withId(R.id.plaintext_createpostfragment_body)).perform(typeText(body)).check(matches(withText(body)));
     }
@@ -53,7 +55,8 @@ public class CreatePostFragmentTest extends FirebaseTest{
     public void cantSendWithoutBody() {
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_myposts));
-        onView(withId(R.id.fragment_my_posts)).perform(click());
+        sleep(500);
+        onView(withId(R.id.floatingbutton_addpost)).perform(click());
         onView(withId(R.id.plaintext_createpostfragment_title)).perform(typeText(title), ViewActions.closeSoftKeyboard()).check(matches(withText(title)));
         onView(withId(R.id.button_createpostfragment_send)).perform(click());
         onView(withText(R.string.createpostfragment_bodyempty)).inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView()))) .check(matches(isDisplayed()));
@@ -63,7 +66,8 @@ public class CreatePostFragmentTest extends FirebaseTest{
     public void canClickButtonWithLongBody() {
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_myposts));
-        onView(withId(R.id.fragment_my_posts)).perform(click());
+        sleep(500);
+        onView(withId(R.id.floatingbutton_addpost)).perform(click());
         onView(withId(R.id.plaintext_createpostfragment_title)).perform(replaceText(longBody)).check(matches(withText(longBody)));
         onView(withId(R.id.plaintext_createpostfragment_body)).perform(replaceText(longBody), ViewActions.closeSoftKeyboard()).check(matches(withText(longBody)));
         onView(withId(R.id.button_createpostfragment_send)).perform(click());
