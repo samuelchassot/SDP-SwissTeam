81aa0520ecf6fcf1b5e667654c0baec253137a5b
sgachoud <sebastien.gachoud@epfl.ch>
2018-10-29 19:35:00 +0100

    changed small implementation in ChatRelation.setFirstUserId and ChatRelation.setSecondUserId

--- app/src/main/java/ch/epfl/swissteam/services/ChatRelation.java
+++ app/src/main/java/ch/epfl/swissteam/services/ChatRelation.java
@@ -18,7 +18,7 @@ public class ChatRelation implements DBSavable {
     private String secondUserId_;
     private String id_;
 
-    public ChatRelation(User firstUser, User secondUser ){
+    public ChatRelation(User firstUser, User secondUser ) {
         this();
         setUsers(firstUser, secondUser);
     }
@@ -41,20 +41,26 @@ public class ChatRelation implements DBSavable {
     }
 
     //Setters
-    public void setFirstUserId_(String user1Id_) {
-        if(secondUserId_ == null){
-            setUsersId(user1Id_, user1Id_);
+    public void setFirstUserId_(String firstUserId) {
+        if(firstUserId == null) {
+            throw new NullPointerException("ChatRelations requires non null firstUser");
+        }
+        if(secondUserId_ == null) {
+            firstUserId_ = firstUserId;
         }
         else{
-            setUsersId(user1Id_, secondUserId_);
+            setUsersId(firstUserId, secondUserId_);
         }
     }
 
-    public void setSecondUserId_(String secondUserId_) {
-        if(firstUserId_ == null){
-            setUsersId(secondUserId_, secondUserId_);
+    public void setSecondUserId_(String secondUserId) {
+        if(secondUserId == null) {
+            throw new NullPointerException("ChatRelations requires non null secondUser");
         }
-        else{
+        if(firstUserId_ == null) {
+            secondUserId_ = secondUserId;
+        }
+        else {
             setUsersId(firstUserId_, secondUserId_);
         }
     }
@@ -63,22 +69,23 @@ public class ChatRelation implements DBSavable {
         this.id_ = id;
     }
 
-    public void setUsers(User firstUser, User secondUser){
-        if(firstUser == null){
+    //public methods
+    public void setUsers(User firstUser, User secondUser) {
+        if(firstUser == null) {
             throw new NullPointerException("ChatRelations requires non null firstUser");
         }
-        if(secondUser == null){
+        if(secondUser == null) {
             throw new NullPointerException("ChatRelations requires non null secondUser");
         }
 
         setUsersId(firstUser.getGoogleId_(), secondUser.getGoogleId_());
     }
 
-    private void setUsersId(String firstId, String secondId){
-        if(firstId == null){
+    private void setUsersId(String firstId, String secondId) {
+        if(firstId == null) {
             throw new NullPointerException("ChatRelations requires non null firstUser googleId");
         }
-        if(secondId == null){
+        if(secondId == null) {
             throw new NullPointerException("ChatRelations requires non null secondUser googleId");
         }
 
@@ -86,25 +93,25 @@ public class ChatRelation implements DBSavable {
             firstUserId_ = firstId;
             secondUserId_ = secondId;
         }
-        else{
+        else {
             firstUserId_ = secondId;
             secondUserId_ = firstId;
         }
     }
 
-    public String getOtherId(String currentUserId){
+    public String getOtherId(String currentUserId) {
         if(!isInThisRelation(currentUserId)) {
             throw new IllegalArgumentException("The current user does not belong to this ChatRelation");
         }
-        if(firstUserId_.compareTo(currentUserId) == 0){
+        if(firstUserId_.compareTo(currentUserId) == 0) {
             return secondUserId_;
         }
-        else{
+        else {
             return firstUserId_;
         }
     }
 
-    private Boolean isInThisRelation(String id){
+    private Boolean isInThisRelation(String id) {
         return firstUserId_.compareTo(id) == 0 || secondUserId_.compareTo(id) == 0;
     }
 
