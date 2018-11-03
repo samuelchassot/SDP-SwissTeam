f8c6717df2f23419cb7c4ab3dd4a5d90ef533f04
abaudat <baudatadrian@gmail.com>
2018-11-03 15:53:04 +0100

    Add test for notifyNewMessages

--- app/src/androidTest/java/ch/epfl/swissteam/services/DBUtilityTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/DBUtilityTest.java
@@ -1,14 +1,34 @@
 package ch.epfl.swissteam.services;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
-public class DBUtilityTest {
+public class DBUtilityTest extends FirebaseTest {
+
+    User testUser;
+
+    @Override
+    public void initialize() {
+        LocationManager.get().setMock();
+        testUser = TestUtils.getTestUser();
+        testUser.addToDB(DBUtility.get().getDb_());
+    }
+
+    @Override
+    public void terminate() {
+        LocationManager.get().unsetMock();
+    }
+
+    @Rule
+    public final ActivityTestRule<MainActivity> mainActivityRule_ =
+            new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void testSingleton() {
@@ -30,5 +50,10 @@ public class DBUtilityTest {
         DBUtility.get().getUsersFromCategory(Categories.ALL,c->{
 
         });
+    }
+
+    @Test
+    public void testNotifyNewMessages() {
+        DBUtility.get().notifyNewMessages(mainActivityRule_.getActivity(), testUser.getGoogleId_());
     }
 }
