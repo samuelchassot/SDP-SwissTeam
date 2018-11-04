85c350ab57424bc5a2bef5d09ee2524347727e07
abaudat <baudatadrian@gmail.com>
2018-11-04 17:55:54 +0100

    Add sleep time to ChatRoomTest

--- app/src/androidTest/java/ch/epfl/swissteam/services/ChatRoomTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/ChatRoomTest.java
@@ -39,13 +39,11 @@ public class ChatRoomTest extends FirebaseTest{
         GoogleSignInSingleton.putUniqueID(TestUtils.M_GOOGLE_ID);
         TestUtils.O_USER.addToDB(DBUtility.get().getDb_());
         TestUtils.M_USER.addToDB(DBUtility.get().getDb_());
-        sleep(100);
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
-        sleep(100);
+        sleep(2000);
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_services));
         sleep(2000);
         recyclerScrollToItemWithTextAndPerformClickItem(R.id.services_recycler, TestUtils.O_USER.getName_());
-        sleep(1000);
         //onView(withId(R.id.button_profile_toChat)).perform(personalClick());
     }
 
