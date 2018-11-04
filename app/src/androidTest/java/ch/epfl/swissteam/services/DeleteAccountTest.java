37c734ea09b8808239167bd4b8cbe2e9eb5cb5b1
samuelchassot <samuel.chassot@epfl.ch>
2018-11-04 20:50:31 +0100

    add test

--- app/src/androidTest/java/ch/epfl/swissteam/services/DeleteAccountTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/DeleteAccountTest.java
@@ -1,9 +1,23 @@
 package ch.epfl.swissteam.services;
 
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.rule.ActivityTestRule;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static ch.epfl.swissteam.services.TestUtils.sleep;
 
 public class DeleteAccountTest extends FirebaseTest {
 
@@ -18,6 +32,10 @@ public class DeleteAccountTest extends FirebaseTest {
         testUser.addToDB(DBUtility.get().getDb_());
         GoogleSignInSingleton.putUniqueID(testUser.getGoogleId_());
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInSingleton.putGoogleSignInClient(GoogleSignIn.getClient(mainActivityRule_.getActivity().getApplicationContext(), gso));
 
     }
 
@@ -28,6 +46,14 @@ public class DeleteAccountTest extends FirebaseTest {
 
     @Test
     public void deleteCurrentUser(){
-        int x =1+1;
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_settings));
+        onView(withId(R.id.button_settings_deleteaccount)).perform(scrollTo()).perform(click());
+        sleep(1000);
+
+        onView(withId(R.id.edittext_deleteaccount_continue)).perform(typeText("CONTINUE"));
+        closeSoftKeyboard();
+        sleep(1000);
+        onView(withId(R.id.button_deleteaccount_deletebutton)).perform(click());
     }
 }
