7f2f2f697f8e0a014760f4794f400cf3def6208d
Julie Giunta <julie.giunta@epfl.ch>
2018-11-05 23:01:27 +0100

    Test switch button

--- app/src/androidTest/java/ch/epfl/swissteam/services/SettingsFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/SettingsFragmentTest.java
@@ -4,6 +4,7 @@ import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.Checkable;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
@@ -34,5 +36,12 @@ public class SettingsFragmentTest {
     public void canOpenSettingsFragment() {
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_settings));
+    }
+
+    @Test
+    public void canSwitchDarkMode() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_settings));
+        onView(withId(R.id.switch_settings_darkmode)).perform(click());
     }
 }
