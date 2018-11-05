171bd9b9ca6a34794a1905a971a3dca1d4d6ac1f
samuelchassot <samuel.chassot@epfl.ch>
2018-11-05 11:12:42 +0100

    add some tests

--- app/src/androidTest/java/ch/epfl/swissteam/services/DeleteAccountTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/DeleteAccountTest.java
@@ -30,6 +30,7 @@ public class DeleteAccountTest extends FirebaseTest {
     @Override
     public void initialize() {
         super.initialize();
+        LocationManager.get().setMock();
         User testUser = TestUtils.getTestUser();
         testUser.addToDB(DBUtility.get().getDb_());
         GoogleSignInSingleton.putUniqueID(testUser.getGoogleId_());
@@ -49,6 +50,30 @@ public class DeleteAccountTest extends FirebaseTest {
     @Test
     public void deleteCurrentUser(){
         User testUser = TestUtils.getTestUser();
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_settings));
+        onView(withId(R.id.button_settings_deleteaccount)).perform(scrollTo()).perform(click());
+        sleep(1000);
+
+        onView(withId(R.id.edittext_deleteaccount_continue)).perform(typeText("CONTINUE"));
+        closeSoftKeyboard();
+        sleep(1000);
+        onView(withId(R.id.button_deleteaccount_deletebutton)).perform(click());
+        sleep(5000);
+
+        DBUtility.get().getUser(testUser.getGoogleId_(), u->{
+            assertEquals(u, null);
+        });
+    }
+
+    @Test
+    public void addPostThenDeleteUser(){
+        initialize();
+        User testUser = TestUtils.getTestUser();
+        Post post = new Post("1234_1539704399119", "Title", testUser.getGoogleId_(), "Body", 1539704399119L,  10, 20);
+        DBUtility.get().setPost(post);
+        sleep(1000);
+
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.button_maindrawer_settings));
         onView(withId(R.id.button_settings_deleteaccount)).perform(scrollTo()).perform(click());
