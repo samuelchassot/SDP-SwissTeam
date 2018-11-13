77745b9e4334ad1e9858617fca588c178c326565
sgachoud <sebastien.gachoud@epfl.ch>
2018-11-13 16:16:14 +0100

    added commments to observer/observable

--- app/src/main/java/ch/epfl/swissteam/services/Observable.java
+++ app/src/main/java/ch/epfl/swissteam/services/Observable.java
@@ -1,5 +1,10 @@
 package ch.epfl.swissteam.services;
 
+/**
+ * Just a simple observable interface
+ * Even if java has a class observable, we needed an interface
+ * @author Sébastien gachoud
+ */
 public interface Observable {
     void addObserver(Observer observer);
 
--- app/src/main/java/ch/epfl/swissteam/services/Observer.java
+++ app/src/main/java/ch/epfl/swissteam/services/Observer.java
@@ -1,5 +1,9 @@
 package ch.epfl.swissteam.services;
 
+/**
+ * Simple observer for our observable
+ * @author Sébastien gachoud
+ */
 public interface Observer {
     void update(Observable observable);
 }
