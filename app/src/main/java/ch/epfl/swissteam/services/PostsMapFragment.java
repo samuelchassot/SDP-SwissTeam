467c87bbeaeab7569587906a4ba32eec4899b0af
abaudat <baudatadrian@gmail.com>
2018-11-15 21:02:18 +0100

    Add info window to markers

--- app/src/main/java/ch/epfl/swissteam/services/PostsMapFragment.java
+++ app/src/main/java/ch/epfl/swissteam/services/PostsMapFragment.java
@@ -1,6 +1,8 @@
 package ch.epfl.swissteam.services;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,6 +12,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,8 +22,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A fragment containing a map showing all surrounding posts.
@@ -138,12 +146,16 @@ public class PostsMapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         googleMap_ = googleMap;
         googleMap_.setMinZoomPreference(12);
+        googleMap_.setInfoWindowAdapter(new CustomInfoWindowAdapter());
         googleMap_.setOnMarkerClickListener(marker -> {
+            marker.showInfoWindow();
+            return false;
+        });
+        googleMap_.setOnInfoWindowClickListener(marker -> {
             Post post = (Post)marker.getTag();
             Intent intent = new Intent(getContext(), PostActivity.class);
             intent.putExtra(PostAdapter.POST_TAG, post);
             startActivity(intent);
-            return false;
         });
         updateMapView();
     }
@@ -151,20 +163,83 @@ public class PostsMapFragment extends Fragment implements OnMapReadyCallback {
     private void updateMapView() {
         Location currentLocation = LocationManager.get().getCurrentLocation_();
 
-        LatLng newLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        googleMap_.moveCamera(CameraUpdateFactory.newLatLng(newLatLng));
+        if(currentLocation != null) {
 
-        Location userLocation = LocationManager.get().getCurrentLocation_();
-        if(userLocation != null) {
+            LatLng newLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            googleMap_.moveCamera(CameraUpdateFactory.newLatLng(newLatLng));
+
             DBUtility.get().getPostsFeed(new DBCallBack<ArrayList<Post>>() {
                 @Override
                 public void onCallBack(ArrayList<Post> value) {
-                    for(Post post : value) {
+                    for (Post post : value) {
                         Marker newMarker = googleMap_.addMarker(new MarkerOptions().position(new LatLng(post.getLatitude_(), post.getLongitude_())));
                         newMarker.setTag(post);
                     }
                 }
-            }, userLocation, new SettingsDbHelper(getContext()));
+            }, currentLocation, new SettingsDbHelper(getContext()));
+        }
+    }
+
+    /**
+     * Info window to show when a marker is clicked on the map
+     *
+     * @author Adrian Baudat
+     */
+    class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+        private final View mContents_;
+
+        private Map<String, User> users = new HashMap<>();
+        private Map<String, Bitmap> pictures = new HashMap<>();
+
+        CustomInfoWindowAdapter() {
+            mContents_ = getLayoutInflater().inflate(R.layout.mapinfo_window, null);
+            DBUtility.get().getAllUsers(list -> {
+                for(User u : list) {
+                    users.put(u.getGoogleId_(), u);
+                    Picasso.get().load(u.getImageUrl_()).into(new Target() {
+                        @Override
+                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                            pictures.put(u.getGoogleId_(), bitmap);
+                        }
+
+                        @Override
+                        public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+                        }
+
+                        @Override
+                        public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                        }
+                    });
+                }
+            });
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            return null;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            Post post = (Post)marker.getTag();
+            ((TextView)mContents_.findViewById(R.id.textview_mapinfo_post)).setText(post.getTitle_());
+            if(users.containsKey(post.getGoogleId_())) {
+                User user = users.get(post.getGoogleId_());
+                ((TextView) mContents_.findViewById(R.id.textview_mapinfo_name)).setText(user.getName_());
+            }
+            else{
+                ((TextView) mContents_.findViewById(R.id.textview_mapinfo_name)).setText(getResources().getString(R.string.mapinfo_unknownuser));
+            }
+            if(pictures.containsKey(post.getGoogleId_())) {
+                Bitmap bitmap = pictures.get(post.getGoogleId_());
+                ((ImageView) mContents_.findViewById(R.id.imageview_mapinfo_picture)).setImageBitmap(bitmap);
+            }
+            else{
+                ((ImageView) mContents_.findViewById(R.id.imageview_mapinfo_picture)).setImageBitmap(null);
+            }
+            return mContents_;
         }
     }
 }
