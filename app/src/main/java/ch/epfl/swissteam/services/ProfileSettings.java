a8d1c30e832efec0c30db796861e83c14085a1c2
Simon Wicky <simon.wicky@epfl.ch>
2018-11-16 11:06:18 +0100

    add darkmode to profileSettings

--- app/src/main/java/ch/epfl/swissteam/services/ProfileSettings.java
+++ app/src/main/java/ch/epfl/swissteam/services/ProfileSettings.java
@@ -35,6 +35,14 @@ public class ProfileSettings extends NavigationDrawer {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        int darkmode = SettingsDBUtility.retrieveDarkMode(this);
+        if (darkmode == 1){
+            getTheme().applyStyle(R.style.DarkMode, true);
+        } else {
+            getTheme().applyStyle(R.style.AppTheme, true);
+        }
+
         setContentView(R.layout.activity_profile_settings);
         super.onCreateDrawer(CANCEL);
 
