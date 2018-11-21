4128274c1acbeff51fd6ee24b80958773b26ac81
Ghali C <ghalicb@gmail.com>
2018-11-21 11:34:32 +0100

    => Add french translation to new strings

--- app/src/main/java/ch/epfl/swissteam/services/SignInActivity.java
+++ app/src/main/java/ch/epfl/swissteam/services/SignInActivity.java
@@ -147,8 +147,8 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
+            CharSequence name = getString(R.string.signin_channel_name);
+            String description = getString(R.string.signin_channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(NotificationUtils.CUSTOM_NOTIFICATION_CHANNEL_ID, name, importance);
             channel.setDescription(description);
