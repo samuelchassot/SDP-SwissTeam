8622ef8761a5141401a41f535203b4ea6e087b1b
Unknown <samuel.chassot@epfl.ch>
2018-11-22 13:54:24 +0100

    add test with recycler view

--- app/src/androidTest/java/ch/epfl/swissteam/services/NewProfileCapabilitiesTest.java
+++ app/src/androidTest/java/ch/epfl/swissteam/services/NewProfileCapabilitiesTest.java
@@ -11,12 +11,22 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static ch.epfl.swissteam.services.TestUtils.personalClick;
+import static ch.epfl.swissteam.services.TestUtils.sleep;
 
 public class NewProfileCapabilitiesTest extends FirebaseTest{
     @Rule
     public final IntentsTestRule<NewProfileCapabilities> mActivityRule =
             new IntentsTestRule<>(NewProfileCapabilities.class);
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        GoogleSignInSingleton.putUniqueID(TestUtils.getTestUser().getGoogleId_());
+    }
 
     @Test
     public void canCheckCapability() {
