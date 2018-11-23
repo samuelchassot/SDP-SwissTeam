e0452de25e82655135390929fab18b594cae07b8
Simon Wicky <simon.wicky@epfl.ch>
2018-11-23 08:48:11 +0100

    close keyboard when creating a post

--- app/src/main/java/ch/epfl/swissteam/services/CreatePostFragment.java
+++ app/src/main/java/ch/epfl/swissteam/services/CreatePostFragment.java
@@ -1,5 +1,6 @@
 package ch.epfl.swissteam.services;
 
+import android.app.Activity;
 import android.location.Location;
 import android.support.v4.app.Fragment;
 
@@ -8,6 +9,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -126,8 +128,9 @@ public class CreatePostFragment extends Fragment implements View.OnClickListener
                 Post post = new Post(key, title, googleID, body, timestamp, longitude, latitude);
                 post.addToDB(DBUtility.get().getDb_());
             });
-
-            ((MainActivity) getActivity()).showHomeFragment();
+            InputMethodManager inputMethodManager = (InputMethodManager)  getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+            ((MainActivity) getActivity()).showMyPostsFragment();
         }
     }
 }
