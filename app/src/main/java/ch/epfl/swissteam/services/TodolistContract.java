9e082098a757a23da14ec6f54c42a4583e1b2b3b
Julie Giunta <julie.giunta@epfl.ch>
2018-12-03 08:28:25 +0100

    Add contract for TODO list DB

---
+++ app/src/main/java/ch/epfl/swissteam/services/TodolistContract.java
@@ -0,0 +1,27 @@
+package ch.epfl.swissteam.services;
+
+import android.provider.BaseColumns;
+
+/**
+ * Contract defining the database table name and column names
+ * for a single table representing the posts in the todolist of the user
+ *
+ * @author Julie Giunta
+ */
+public class TodolistContract {
+    public static final String LOCAL_DB_NAME = "ch.epfl.swissteam.services.todolistlocalDB";
+    public static final int DB_VERSION = 1;
+
+    private TodolistContract(){}
+
+    public static class TodolistEntry implements BaseColumns{
+        public static final String TABLE_NAME = "entry";
+
+        //Column for the id of the logged in user
+        public static final String COLUMN_ID = "id";
+
+        //Column for the key of the post
+        public static final String COLUMN_POSTS = "post";
+
+    }
+}
