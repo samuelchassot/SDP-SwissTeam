344a4540c879daab96d91b3af6c1804f674dea3d
Julie Giunta <julie.giunta@epfl.ch>
2018-12-04 13:19:25 +0100

    Create TodoListFragmentTest

---
+++ app/src/androidTest/java/ch/epfl/swissteam/services/TodoListFragmentTest.java
@@ -0,0 +1,8 @@
+package ch.epfl.swissteam.services;
+
+public class TodoListFragmentTest extends SocializeTest<MainActivity> {
+
+    public TodoListFragmentTest(){
+        setTestRule(MainActivity.class);
+    }
+}
