da89b3106775ccb5bd6e6fdb2fba6284129a6438
Julie Giunta <julie.giunta@epfl.ch>
2018-12-12 15:23:48 +0100

    Refactor TodoListDBUtility

--- app/src/main/java/ch/epfl/swissteam/services/providers/TodoListDBUtility.java
+++ app/src/main/java/ch/epfl/swissteam/services/providers/TodoListDBUtility.java
@@ -6,33 +6,30 @@ import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import ch.epfl.swissteam.services.models.Post;
-import ch.epfl.swissteam.services.providers.DBCallBack;
-import ch.epfl.swissteam.services.providers.DBUtility;
 import ch.epfl.swissteam.services.utils.TodoListContract;
 import ch.epfl.swissteam.services.utils.TodoListDbHelper;
 
 
 /**
- * Interface that provides some static methods to interact with the local DB for the posts in the TodoList.
+ * Class that provides some static methods to interact with the local DB for the posts in the TodoList.
  *
  * @author Julie Giunta
  */
 public class TodoListDBUtility {
 
     /**
-     * Add a row in the local TodolistDB which has userID and postID as values.
+     * Add a row in the local TodoListDB which has userID and postID as values.
      * It adds the row only if there is not a row with these values yet
      *
-     * @param helper a TodolistDBHelper created with the context of the caller,
+     * @param helper a TodoListDBHelper created with the context of the caller,
      * @param userID the id of the currently logged in user,
      * @param postID the key of the post to add to the local DB.
-     *
      * @return the key of the new row added to the local DB,
      * 0 if it was already in the DB, -1 if there was a problem adding the row.
      */
-    public static long addPost(TodoListDbHelper helper, String userID, String postID){
+    public static long addPost(TodoListDbHelper helper, String userID, String postID) {
         long rowKey = 0;
-        if(!isPostInDB(helper, userID, postID)){
+        if (!isPostInDB(helper, userID, postID)) {
             SQLiteDatabase db = helper.getWritableDatabase();
 
             //Value which will be stored as a row in the local DB
@@ -50,22 +47,21 @@ public class TodoListDBUtility {
     }
 
     /**
-     * Deletes rows in the local TodolistDB which have userID and postID as values.
+     * Deletes rows in the local TodoListDB which have userID and postID as values.
      *
-     * @param helper a TodolistDBHelper created with the context of the caller,
+     * @param helper a TodoListDBHelper created with the context of the caller,
      * @param userID the id of the currently logged in user,
      * @param postID the key of the post to add to the local DB.
-     *
      * @return the number of rows deleted.
      */
-    public static int deletePost(TodoListDbHelper helper, String userID, String postID){
+    public static int deletePost(TodoListDbHelper helper, String userID, String postID) {
         SQLiteDatabase db = helper.getWritableDatabase();
 
         // Define 'where' part of query.
         String selection = TodoListContract.TodolistEntry.COLUMN_ID + " LIKE ? AND "
                 + TodoListContract.TodolistEntry.COLUMN_POSTS + " LIKE ? ";
         // Specify arguments in placeholder order.
-        String[] selectionArgs = { userID, postID };
+        String[] selectionArgs = {userID, postID};
 
         int deletedRows = db.delete(TodoListContract.TodolistEntry.TABLE_NAME, selection, selectionArgs);
         db.close();
@@ -74,70 +70,72 @@ public class TodoListDBUtility {
     }
 
     /**
-     * Retrieve the posts that the user has stocked in his todolist.
+     * Retrieve the posts that the user has stocked in his TodoList.
      *
-     * @param helper a TodolistDBHelper created with the context of the caller,
-     * @param userID the id of the currently logged in user,
+     * @param helper   a TodoListDBHelper created with the context of the caller,
+     * @param userID   the id of the currently logged in user,
      * @param callback a callback function to use whenever we retrieve a post.
      */
-    public static void getPosts(TodoListDbHelper helper, String userID, DBCallBack<Post> callback){
+    public static void getPosts(TodoListDbHelper helper, String userID, DBCallBack<Post> callback) {
         SQLiteDatabase db = helper.getReadableDatabase();
 
         String[] projection = {TodoListContract.TodolistEntry.COLUMN_POSTS};
 
         String selection = TodoListContract.TodolistEntry.COLUMN_ID + " = ?";
-        String[] selectionArgs = { userID };
+        String[] selectionArgs = {userID};
 
-        try{
+        try {
             Cursor cursor = db.query(
                     TodoListContract.TodolistEntry.TABLE_NAME,
                     projection, selection, selectionArgs,
                     null, null, null
             );
 
-            while(cursor.moveToNext()){
+            while (cursor.moveToNext()) {
                 String postID = cursor.getString(
                         cursor.getColumnIndexOrThrow(TodoListContract.TodolistEntry.COLUMN_POSTS));
-                DBUtility.get().getPost(postID, p -> callback.onCallBack(p));
+                DBUtility.get().getPost(postID, callback);
             }
 
             cursor.close();
-        }catch(IllegalArgumentException e){
-            Log.e("TODOLISTDBUTILITY", "Column does not exist when retrieving posts");
-        }catch(Exception e){
-            Log.e("TODOLISTDBUTILITY", "Could not query the DB");
+        } catch (IllegalArgumentException e) {
+            Log.e("TODO_LIST_DB_UTILITY", "Column does not exist when retrieving posts");
+        } catch (Exception e) {
+            Log.e("TODO_LIST_DB_UTILITY", "Could not query the DB");
         }
 
         db.close();
     }
 
-    public static boolean isPostInDB(TodoListDbHelper helper, String userID, String postID){
+    /**
+     * Check if a post is already in the database of the currently logged in user.
+     *
+     * @param helper a TodoListDbHelper created with the context of the caller,
+     * @param userID the id of the currently logged in user,
+     * @param postID the id of the post,
+     * @return true if the post is in the local database, false otherwise.
+     */
+    public static boolean isPostInDB(TodoListDbHelper helper, String userID, String postID) {
         SQLiteDatabase db = helper.getReadableDatabase();
 
         String[] projection = {
                 TodoListContract.TodolistEntry.COLUMN_POSTS
         };
 
-        // Define 'where' part of query.
         String selection = TodoListContract.TodolistEntry.COLUMN_ID + " = ? AND "
                 + TodoListContract.TodolistEntry.COLUMN_POSTS + " = ? ";
-        // Specify arguments in placeholder order.
-        String[] selectionArgs = { userID, postID };
 
+        String[] selectionArgs = {userID, postID};
 
         Cursor cursor = db.query(
-                TodoListContract.TodolistEntry.TABLE_NAME,   // The table to query
-                projection,             // The array of columns to return (pass null to get all)
-                selection,              // The columns for the WHERE clause (selection)
-                selectionArgs,          // The values for the WHERE clause (selectionArgs)
-                null,                   // don't group the rows
-                null,                   // don't filter by row groups
-                null              // The sort order (sortOrder)
+                TodoListContract.TodolistEntry.TABLE_NAME,
+                projection, selection, selectionArgs,
+                null, null, null
         );
 
         boolean exists = false;
 
-        if(cursor.moveToFirst()){
+        if (cursor.moveToFirst()) {
             exists = true;
         }
         cursor.close();
